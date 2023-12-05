066f978253734e14839c9e0d4163734829d186a4
John Hamre <jjha2020@mymail.pomona.edu>
2023-12-04 21:18:41 -0800

    Finished main game

--- core/game1/src/main.rs
+++ core/game1/src/main.rs
@@ -9,20 +9,11 @@ pub use engine::structs::Arrow;
 pub use std::collections::VecDeque;
 const W: f32 = 320.0;
 const H: f32 = 240.0;
-const GUY_SPEED: f32 = 4.0;
 const SPRITE_MAX: usize = 16;
-const CATCH_DISTANCE: f32 = 16.0;
-const COLLISION_STEPS: usize = 3;
-
-struct Guy {
-    pos: Vec2,
-}
+const MERCIES: (i32, i32, i32) = (10, 20, 30);
 
 struct GameState {
-    walls: Vec<AABB>,
-    guy: Guy,
     arrows: Vec<Arrow>,
-    apple_timer: u32,
     score: u32,
 }
 
@@ -61,27 +52,6 @@ impl engine::Game for Game {
             vec![SheetRegion::zeroed(); SPRITE_MAX],
             camera,
         );
-        let guy = Guy {
-            pos: Vec2 {
-                x: W / 2.0,
-                y: 24.0,
-            },
-        };
-        let floor = AABB {
-            center: Vec2 { x: W / 2.0, y: 8.0 },
-            size: Vec2 { x: W, y: 16.0 },
-        };
-        let left_wall = AABB {
-            center: Vec2 { x: 8.0, y: H / 2.0 },
-            size: Vec2 { x: 16.0, y: H },
-        };
-        let right_wall = AABB {
-            center: Vec2 {
-                x: W - 8.0,
-                y: H / 2.0,
-            },
-            size: Vec2 { x: 16.0, y: H },
-        };
 
         let font = engine::BitFont::with_sheet_region(
             '0'..='9',
@@ -91,105 +61,51 @@ impl engine::Game for Game {
         Game {
             camera,
             gamestate: GameState {
-                guy,
-                walls: vec![left_wall, right_wall, floor],
                 arrows: Vec::with_capacity(16),
-                apple_timer: 0,
                 score: 0,
             },
             font,
         }
     }
     fn update(&mut self, engine: &mut Engine, frame_events: &mut VecDeque<REvent>) {
-        let dir = engine.input.key_axis(engine::Key::Left, engine::Key::Right);
-        self.gamestate.guy.pos.x += dir * GUY_SPEED;
+        // Input catcher for each arrow type
+        if  engine.input.is_key_pressed(engine::Key::A) || 
+            engine.input.is_key_pressed(engine::Key::Left) {
+                update_score(self, engine, 0);
+        }
+
+        if  engine.input.is_key_pressed(engine::Key::S) || 
+        engine.input.is_key_pressed(engine::Key::Down) {
+            update_score(self, engine, 1);
+        }
+
+        if  engine.input.is_key_pressed(engine::Key::W) || 
+        engine.input.is_key_pressed(engine::Key::Up) {
+            update_score(self, engine, 2);
+        }
+
+        if  engine.input.is_key_pressed(engine::Key::D) || 
+        engine.input.is_key_pressed(engine::Key::Right) {
+            update_score(self, engine, 3);
+        }
+
+        // Spawn new events per frame.
         for event in frame_events.drain(..) {
             spawn_event(self, event);
         }
-        let mut contacts = Vec::with_capacity(self.gamestate.walls.len());
-        // TODO: for multiple guys this might be better as flags on the guy for what side he's currently colliding with stuff on
-        for _iter in 0..COLLISION_STEPS {
-            let guy_aabb = AABB {
-                center: self.gamestate.guy.pos,
-                size: Vec2 { x: 16.0, y: 16.0 },
-            };
-            contacts.clear();
-            // TODO: to generalize to multiple guys, need to iterate over guys first and have guy_index, rect_index, displacement in a contact tuple
-            contacts.extend(
-                self.gamestate.walls
-                    .iter()
-                    .enumerate()
-                    .filter_map(|(ri, w)| w.displacement(guy_aabb).map(|d| (ri, d))),
-            );
-            if contacts.is_empty() {
-                break;
-            }
-            // This part stays mostly the same for multiple guys, except the shape of contacts is different
-            contacts.sort_by(|(_r1i, d1), (_r2i, d2)| {
-                d2.length_squared()
-                    .partial_cmp(&d1.length_squared())
-                    .unwrap()
-            });
-            for (wall_idx, _disp) in contacts.iter() {
-                // TODO: for multiple guys should access self.guys[guy_idx].
-                let guy_aabb = AABB {
-                    center: self.gamestate.guy.pos,
-                    size: Vec2 { x: 16.0, y: 16.0 },
-                };
-                let wall = self.gamestate.walls[*wall_idx];
-                let mut disp = wall.displacement(guy_aabb).unwrap_or(Vec2::ZERO);
-                // We got to a basically zero collision amount
-                if disp.x.abs() < std::f32::EPSILON || disp.y.abs() < std::f32::EPSILON {
-                    break;
-                }
-                // Guy is left of wall, push left
-                if self.gamestate.guy.pos.x < wall.center.x {
-                    disp.x *= -1.0;
-                }
-                // Guy is below wall, push down
-                if self.gamestate.guy.pos.y < wall.center.y {
-                    disp.y *= -1.0;
-                }
-                if disp.x.abs() <= disp.y.abs() {
-                    self.gamestate.guy.pos.x += disp.x;
-                    // so far it seems resolved; for multiple guys this should probably set a flag on the guy
-                } else if disp.y.abs() <= disp.x.abs() {
-                    self.gamestate.guy.pos.y += disp.y;
-                    // so far it seems resolved; for multiple guys this should probably set a flag on the guy
-                }
-            }
-        }
-        //let mut rng = rand::thread_rng();
-        if self.gamestate.apple_timer > 0 {
-            self.gamestate.apple_timer -= 1;
-        } else if self.gamestate.arrows.len() < 8 {
-            // spawn_arrow( 
-            //     self,
-            //     Vec2 {
-            //         //x: rng.gen_range(8.0..(W - 8.0)),
-            //         x: W/2.0,
-            //         y: H + 8.0,
-            //     },
-            //     Vec2 {
-            //         x: 0.0,
-            //         //y: rng.gen_range((-4.0)..(-1.0)),
-            //         y: -1.0,
-            //     },
-            // 0.0,0.0,1,0);
-            self.gamestate.apple_timer = 100;
-        }
+        // Move arrows per frame.
         for arrow in self.gamestate.arrows.iter_mut() {
             arrow.pos += arrow.vel;
         }
-        if let Some(idx) = self.gamestate
-            .arrows
-            .iter()
-            .position(|arrow: &Arrow| arrow.pos.distance(self.gamestate.guy.pos) <= CATCH_DISTANCE)
-        {
-            self.gamestate.arrows.swap_remove(idx);
-            self.gamestate.score += 1;
+        // Clear arrows that have left the world
+        let initlen = self.gamestate.arrows.len();
+        self.gamestate.arrows.retain(|apple| apple.pos.y < H + 8.0);
+        if initlen > self.gamestate.arrows.len() {
+            for _ in 0..(initlen - self.gamestate.arrows.len())
+            {
+                self.gamestate.score = subtract_with_floor(self.gamestate.score, 500);
+            }
         }
-        self.gamestate.arrows.retain(|apple| apple.pos.y > -8.0)
     }
     fn render(&mut self, engine: &mut Engine) {
         let (trfs, uvs) = engine.renderer.sprites.get_sprites_mut(0);
@@ -240,27 +156,8 @@ impl engine::Game for Game {
         }
         .into();
         uvs[4] = SheetRegion::new(0, 0, 0, 16, 880, 440);
-        // set walls
-        const WALL_START: usize = 5;
-        let guy_idx = WALL_START + self.gamestate.walls.len();
-        for (wall, (trf, uv)) in self.gamestate.walls.iter().zip(
-            trfs[WALL_START..guy_idx]
-                .iter_mut()
-                .zip(uvs[WALL_START..guy_idx].iter_mut()),
-        ) {
-            *trf = (*wall).into();
-            *uv = SheetRegion::new(0, 0, 480, 12, 0, 0);
-        }
-        // set guy
-        trfs[guy_idx] = AABB {
-            center: self.gamestate.guy.pos,
-            size: Vec2 { x: 16.0, y: 16.0 },
-        }
-        .into();
-        // TODO animation frame
-        uvs[guy_idx] = SheetRegion::new(0, 16, 480, 8, 16, 16);
         // set apple
-        let apple_start = guy_idx + 1;
+        let apple_start = 5;
         for (apple, (trf, uv)) in self.gamestate.arrows.iter().zip(
             trfs[apple_start..]
                 .iter_mut()
@@ -312,7 +209,6 @@ impl engine::Game for Game {
 
 fn spawn_arrow(game: &mut Game, arrow: Arrow) {
     game.gamestate.arrows.push(arrow);
-    println!("{:?}", game.gamestate.arrows.get(0).unwrap());
 }
 
 fn spawn_event(game: &mut Game, event: REvent) {
@@ -322,6 +218,58 @@ fn spawn_event(game: &mut Game, event: REvent) {
         }
         _ => {}
     }
+}
+
+fn update_score(game: &mut Game, engine: &mut Engine, arrow_dir: usize) {
+    let indices0 = game.gamestate.arrows
+    .iter()
+    .enumerate()
+    .filter(|(_, r)| 
+        ((r.target_time - engine.level_state.queue_timer).abs() < MERCIES.0) && 
+        (r.arrow_dir == arrow_dir)
+    )
+    .map(|(index, _)| index)
+    .collect::<Vec<_>>();
+
+    let indices1 = game.gamestate.arrows
+    .iter()
+    .enumerate()
+    .filter(|(_, r)| 
+        ((r.target_time - engine.level_state.queue_timer).abs() < MERCIES.1) && 
+        (r.arrow_dir == arrow_dir)
+    )
+    .map(|(index, _)| index)
+    .collect::<Vec<_>>();
+    
+    let indices2 = game.gamestate.arrows
+    .iter()
+    .enumerate()
+    .filter(|(_, r)| 
+        ((r.target_time - engine.level_state.queue_timer).abs() < MERCIES.2) && 
+        (r.arrow_dir == arrow_dir)
+    )
+    .map(|(index, _)| index)
+    .collect::<Vec<_>>();
+
+    if indices0.len() > 0 {
+        game.gamestate.arrows.remove(indices0[0]);
+        game.gamestate.score += 1000;
+    }
+    else if indices1.len() > 0 {
+        game.gamestate.arrows.remove(indices1[0]);
+        game.gamestate.score += 500;
+    }
+    else if indices2.len() > 0 {
+        game.gamestate.arrows.remove(indices2[0]);
+        game.gamestate.score += 100;
+    }
+    else {
+        game.gamestate.score = subtract_with_floor(game.gamestate.score, 500);
+    }
+}
+
+fn subtract_with_floor(num: u32, amount: i32) -> u32 {
+    return if num as i32 - amount < 0 {0} else {(num as i32 - amount) as u32};
 }
 
 fn main() {
